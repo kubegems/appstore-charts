84ac861daf480569774a59d1ad71512c33f931cf
yu.deng <dengyu326@gmail.com>
2022-03-22 10:57:26 +0800

    update upload.sh; support tenant name from environment

--- upload.sh
+++ upload.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 code=0
+HOST=${CHARTMUSEUM_HOST:-"gems-chartmuseum:8030"}
+INNER=${KUBEGEMS_INNER_REPONAME:-"kubegems-appstore"}
 while [ ! $code == 200 ];do
-     code=`curl -sL -w "%{http_code}\\n" "http://gems-chartmuseum:8030" -o /dev/null`
+     code=`curl -sL -w "%{http_code}\\n" "http://${HOST}" -o /dev/null`
+     echo "`date` chartmuseum not start; wait 1s"
      sleep 1
 done
 
@@ -10,8 +13,8 @@ done
 echo "chartmuseum is available"
 
 for chart in `ls charts`;do
-    curl --data-binary "@charts/${chart}" http://gems-chartmuseum:8030/api/kubegemsapp/charts
-    echo "------------------------------"
+    curl --data-binary "@charts/${chart}" http://${HOST}/api/${INNER}/charts
+    echo "\n------------------------------"
 done
 
 exit 0
