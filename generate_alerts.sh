0a37237e9c0c34f765a17b508aafeeb0125e10d1
sunlintong <sunlintongshuai@gmail.com>
2022-11-03 13:22:19 +0800

    rm kafka alerts

--- generate_alerts.sh
+++ generate_alerts.sh
@@ -5,7 +5,7 @@ ROOTDIR='.'
 exporters=$(find ${ROOTDIR} -maxdepth 1 -mindepth 1 -type d -name "prometheus-*-exporter" -printf '%f\n')
 
 for exporter in $exporters; do
-    if [ $exporter != "prometheus-redis-exporter" ]; then
+    if [ $exporter != "prometheus-kafka-exporter" ]; then
    	    go run scripts/gen-alerts/main.go --exporter $exporter
 	else
 	    continue
